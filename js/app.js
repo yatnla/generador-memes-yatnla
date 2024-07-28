4a220e500305a1b7734849bb534518f28e7a7f4f
yatnla <yatnlayatn@gmail.com>
2024-07-28 18:07:46 -0300

    Se agregaron estilos al aside de txt.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const textButton = document.getElementById('txt-button');
 const imageSection = document.getElementById('img-section');
 const textSection = document.getElementById('txt-section');
 
+const darkModeButton = document.getElementById('dark-button');
+
 const memeContainer = document.getElementById('meme-container');
 const downloadButton = document.getElementById('download-button');
 
@@ -26,20 +28,59 @@ const invertImg = document.getElementById('invert');
 
 const resetButton = document.getElementById('reset-button');
 
+const topTxt = document.getElementById('top-txt');
+const topTxtMeme = document.getElementById('top-txt-meme');
+const bottomTxt = document.getElementById('bottom-txt');
+const bottomTxtMeme = document.getElementById('bottom-txt-meme');
 
-// toggle img and txt aside w/ func.
+const topTxtCheck = document.getElementById('top-txt-check');
+const bottomTxtCheck = document.getElementById('bottom-txt-check');
 
-function toggleSections() {
-    if (imageSection.style.display === 'block') {
-        imageSection.style.display = 'none';
-        textSection.style.display = 'block';
-    } else {
-        imageSection.style.display = 'block';
-        textSection.style.display = 'none';
-    }
-}
-textButton.addEventListener('click', toggleSections);
-imageButton.addEventListener('click', toggleSections);
+const fontFamily = document.getElementById('font');
+
+const fontSize = document.getElementById('font-size');
+
+const leftAlign = document.getElementById('left-align');
+const centerAlign = document.getElementById('center-align');
+const rightAlign = document.getElementById('right-align');
+
+const colorTxt = document.getElementById('color-txt');
+const colorTxtValue = document.getElementById('color-txt-value');
+
+const bgColorTxt = document.getElementById('bg-color-txt');
+const bgColorTxtValue = document.getElementById('bg-color-txt-value');
+
+const bgTransparentCheck = document.getElementById('bg-transparent-check');
+
+const noneOutline = document.getElementById('none-outline');
+const lightOutline = document.getElementById('light-outline');
+const darkOutline = document.getElementById('dark-outline');
+
+const paddingTxt = document.getElementById('padding');
+
+const lineSpacing = document.getElementById('line-spacing');
+
+// header section -->
+
+// toggle img and txt
+
+imageButton.addEventListener('click', () => {
+    imageSection.style.display = imageSection.style.display === 'block' ? 'none' : 'block';
+    textSection.style.display = 'none';
+});
+
+textButton.addEventListener('click', () => {
+    textSection.style.display = textSection.style.display === 'block' ? 'none' : 'block';
+    imageSection.style.display = 'none';
+});
+
+// light and dark mode
+
+darkModeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+// canvas section -->
 
 // download button
 
@@ -93,7 +134,8 @@ invertImg.addEventListener('change', filterImg);
 
 // reset button
 
-resetButton.addEventListener('click', () => {
+resetButton.addEventListener('click', (e) => {
+    e.preventDefault();
     brightnessImg.value = 1;
     opacityImg.value = 1;
     contrastImg.value = 100;
@@ -103,5 +145,146 @@ resetButton.addEventListener('click', () => {
     hueRotateImg.value = 0;
     saturationImg.value = 100;
     invertImg.value = 0;
-}
-)
+    filterImg()
+})
+
+// txt section -->
+
+// top txt
+
+topTxt.addEventListener('keyup', () => {    
+    topTxtMeme.innerHTML = topTxt.value;    
+});
+
+// bottom txt
+
+bottomTxt.addEventListener('keyup', () => {    
+    bottomTxtMeme.innerHTML = bottomTxt.value;    
+});
+
+// top txt check
+
+topTxtCheck.addEventListener('click', () => {
+    topTxtMeme.style.display = topTxtMeme.style.display === 'none' ? 'block' : 'none';
+})
+
+// bottom txt check
+
+bottomTxtCheck.addEventListener('click', () => {
+    bottomTxtMeme.style.display = bottomTxtMeme.style.display === 'none' ? 'block' : 'none';
+})
+
+// font family
+
+fontFamily.addEventListener('change', () => {
+    topTxtMeme.style.fontFamily = `${fontFamily.value}`;
+    bottomTxtMeme.style.fontFamily = `${fontFamily.value}`;
+});
+
+// font size
+
+fontSize.addEventListener('change', () => {
+    topTxtMeme.style.fontSize = `${fontSize.value}px`;
+    bottomTxtMeme.style.fontSize = `${fontSize.value}px`;
+});
+
+// justify txt
+
+leftAlign.addEventListener('click', (e) => {
+    topTxtMeme.style.textAlign = 'left';
+    bottomTxtMeme.style.textAlign = 'left';  
+});
+
+centerAlign.addEventListener('click', (e) => {
+    topTxtMeme.style.textAlign = 'center';
+    bottomTxtMeme.style.textAlign = 'center';  
+});
+
+rightAlign.addEventListener('click', (e) => { 
+    topTxtMeme.style.textAlign = 'right';
+    bottomTxtMeme.style.textAlign = 'right';  
+});
+
+// color txt
+
+colorTxt.addEventListener('input', (e) => {
+    let value = e.target.value;   
+    topTxtMeme.style.color = `${value}`;
+    bottomTxtMeme.style.color = `${value}`;
+    colorTxtValue.innerHTML = value.toUpperCase();   
+});
+
+// bg txt color
+
+bgColorTxt.addEventListener('input', (e) => {
+    let value = e.target.value;   
+    topTxtMeme.style.backgroundColor = `${value}`;
+    bottomTxtMeme.style.backgroundColor = `${value}`;
+    bgColorTxtValue.innerHTML = value.toUpperCase();   
+});
+
+// bg txt transparent
+
+bgTransparentCheck.addEventListener('click', (e) => {
+    if(e.target.checked){
+        topTxtMeme.style.backgroundColor = 'transparent';
+        bottomTxtMeme.style.backgroundColor = 'transparent';
+        topTxtMeme.style.position = 'absolute';
+        bottomTxtMeme.style.position = 'absolute';
+        topTxtMeme.style.top = '0';
+        bottomTxtMeme.style.bottom = '0';
+        topTxtMeme.style.left = '50%';
+        topTxtMeme.style.zIndex = '1';
+        bottomTxtMeme.style.left = '50%';
+        topTxtMeme.style.transform = 'translateX(-50%)';
+        bottomTxtMeme.style.transform = 'translateX(-50%)';
+        topTxtMeme.style.width = '100%';
+        bottomTxtMeme.style.width = '100%';
+    } else {
+        topTxtMeme.style.backgroundColor = '';
+        bottomTxtMeme.style.backgroundColor = '';
+        topTxtMeme.style.position = '';
+        bottomTxtMeme.style.position = '';
+        topTxtMeme.style.top = '';
+        bottomTxtMeme.style.bottom = '';
+        topTxtMeme.style.left = '';
+        topTxtMeme.style.zIndex = '';
+        bottomTxtMeme.style.left = '';
+        topTxtMeme.style.transform = '';
+        bottomTxtMeme.style.transform = '';
+        topTxtMeme.style.width = '';
+        bottomTxtMeme.style.width = '';
+    }
+});
+
+// outline txt
+
+noneOutline.addEventListener('click', () => {
+    topTxtMeme.style.webkitTextStroke = '';
+    bottomTxtMeme.style.webkitTextStroke = '';
+});
+
+lightOutline.addEventListener('click', () => {
+    topTxtMeme.style.webkitTextStroke = '1px white';
+    bottomTxtMeme.style.webkitTextStroke = '1px white';
+});
+
+darkOutline.addEventListener('click', () => {
+    topTxtMeme.style.webkitTextStroke = '1px black';
+    bottomTxtMeme.style.webkitTextStroke = '1px black';
+});
+
+// padding txt
+
+paddingTxt.addEventListener('input', () => {
+    topTxtMeme.style.padding = `calc(5% + ${paddingTxt.value}px)`;
+    bottomTxtMeme.style.padding = `calc(5% + ${paddingTxt.value}px)`;
+});
+
+// line spacing txt
+
+lineSpacing.addEventListener('change', (e) => {
+    let value = e.target.value;
+    topTxtMeme.style.lineHeight = value;
+    bottomTxtMeme.style.lineHeight = value;
+});
